aa5f39839d91e738c0ba8f328532fbfd156331af
Ramie <ramiebash@gmail.com>
2024-10-29 09:25:32 -0500

    added helmet code in server.js

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');  // Import helmet
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// Apply Helmet and set CSP
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://clinic-website.azurewebsites.net"],  // Add your domain
+      // You can add other CSP directives here if needed
+    },
+  })
+);
 
 // Import routes
 const db = require('./backend/db');
